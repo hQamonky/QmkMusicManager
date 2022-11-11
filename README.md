4eadb632612441a2a2a72c1d290fc080ead122c2
qmk <wherlicq@gmail.com>
2022-11-11 15:19:16 +0100

    Patch 1.0.1.

--- README.md
+++ README.md
@@ -1,7 +1,27 @@
 # QmkMusicManager
-Download and manage music through a REST API.
+QMK Music Manager is a REST API to download and manage music.  
+[Here](https://github.com/hQamonky/QmkMusicManager/blob/master/docs/Api%20User%20Guide.md)
+is the API reference and list of features.  
+It is a Kotlin application packaged in a jar file.
+## Requirements
+- You need to have java 11 installed in order to run the application.  
+- You also need to have [youtube-dl](https://youtube-dl.org/) installed.  
 
-[Here is the API reference](https://github.com/hQamonky/QmkMusicManager/blob/master/docs/Api%20User%20Guide.md).
-
-
-# Installation
+Alternatively you can create a docker image and run it in a container (the container will need java 11 and youtube-dl installed).
+## Installation
+There is an installation script for linux that creates a systemd service.  
+For other operating systems, you will have to install it manually (see below).
+### Using the installation script
+#### Download using the terminal
+``` shell
+wget -P ~/Downloads https://raw.githubusercontent.com/hQamonky/QmkMusicManager/master/installers/linux_installer.sh
+chmod +x ~/Downloads/linux_installer.sh
+sudo ~/Downloads/linux_installer.sh -u qmk
+```
+For the last command, replace "qmk" by your username.  
+The script must run as sudo because is uses systemctl commands.  
+You can also download the script through a web browser by visiting [this page](https://raw.githubusercontent.com/hQamonky/QmkMusicManager/master/installers/linux_installer.sh),
+right-clicking anywhere on the page and clicking "Save Page As...".
+### Manual installation
+Download the jar package from [here](https://raw.githubusercontent.com/hQamonky/QmkMusicManager/master/package/musicmanager-1.0.1.jar).
+Then you can run it with `java -jar path/to/musicmanager-*.jar`.
--- installers/linux_installer.sh
+++ installers/linux_installer.sh
@@ -2,31 +2,27 @@
 
 username=$USER
 installDir=/opt/qmk
-workDir=/home/$username/qmk-tmp-workdir
 
 Help()
 {
    # Display Help
    echo "Description: Install QMK Music Manager as a systemd daemon."
    echo
-   echo "Syntax: linux_installer.sh [-h|u|d|w]"
+   echo "Syntax: linux_installer.sh [-h|u|d]"
    echo "options:"
    echo "h     Print this help."
    echo "u     User used to run the installed service. Default user is $username."
    echo "d     Directory where the application will be installed. Default location is $installDir/."
-   echo "w     Temporary directory. Default directory is $workDir."
    echo
 }
 
-while getopts :h:u:d:w: flag
+while getopts :h:u:d: flag
 do
     case "${flag}" in
         h) Help
           exit;;
-        u) username=${OPTARG}
-          workDir=/home/$username/qmk-tmp-workdir;;
+        u) username=${OPTARG};;
         d) installDir=${OPTARG};;
-        w) workDir=${OPTARG};;
         *) echo "Invalid option."
           echo
           Help
@@ -36,16 +32,12 @@ done
 
 echo "Service will run under $username user.";
 echo "Will install package to $installDir.";
-echo "Temporary directory $workDir will be created (and deleted).";
 
-echo "Creating necessary directories..."
+echo "Creating installation directory..."
 mkdir -p "$installDir"
-mkdir -p "$workDir"
 
 echo "Fetching application..."
-git -C "$workDir" clone https://github.com/hQamonky/QmkMusicManager.git
-cp "$workDir"/QmkMusicManager/package/musicmanager-1.0.0.jar "$installDir"/musicmanager-1.0.0.jar
-rm -rf "$workDir"
+wget -P "$installDir" https://raw.githubusercontent.com/hQamonky/QmkMusicManager/master/package/musicmanager-1.0.1.jar
 
 echo "Creating service..."
 serviceFile=/etc/systemd/system/qmk_music_manager.service
@@ -56,7 +48,7 @@ echo >> $serviceFile
 echo "[Service]" >> $serviceFile
 echo "User=$username" >> $serviceFile
 echo "WorkingDirectory=$installDir" >> $serviceFile
-echo "ExecStart=java -jar $installDir/musicmanager-1.0.0.jar" >> $serviceFile
+echo "ExecStart=java -jar $installDir/musicmanager-1.0.1.jar" >> $serviceFile
 echo "Restart=always" >> $serviceFile
 echo >> $serviceFile
 echo "[Install]" >> $serviceFile
@@ -64,7 +56,7 @@ echo "WantedBy=multi-user.target" >> $serviceFile
 
 systemctl daemon-reload
 systemctl enable qmk_music_manager.service
-systemctl start qmk_music_manager.service
+systemctl restart qmk_music_manager.service
 
 echo "Installation finished."
 
