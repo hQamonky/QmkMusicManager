383a866f0e75cf81d91b70fec31dbf5b00838c38
hQamonky <44162955+hQamonky@users.noreply.github.com>
2022-12-06 17:24:23 +0100

    Update Settings.kt

--- src/main/kotlin/com/qmk/musicmanager/model/Settings.kt
+++ src/main/kotlin/com/qmk/musicmanager/model/Settings.kt
@@ -7,11 +7,11 @@ import io.swagger.v3.oas.annotations.media.Schema
 data class Settings(
     @field:Schema(
         description = "The interval of time (in minutes) at which playlists are automatically downloaded. Has no effect if autoDownload is set to false.",
-        example = "1",
+        example = "60",
         type = "Int",
         minimum = "1"
     )
-    val downloadOccurrence: Int = 1,
+    val downloadOccurrence: Int = 60,
     @field:Schema(
         description = "The path of the music folder on the server.",
         example = "/home/qmk/Music",
